c71cd171b3d1578040e92a362103d26864c8a0f2
seppe <modsahilprof0@gmail.com>
2026-05-14 13:55:41 +0530

    first commit

---
+++ apps/accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.username
